50d95c4ac8d09b5face2ce4ea9a49f143252bad6
Adrian <confuzedfx@gmail.com>
2021-02-15 18:05:43 -0800

    Lab5 done

---
+++ app.js
@@ -0,0 +1,58 @@
+/*
+ Authors:
+ Your name and student #:Adrian McFarlane A00957549
+ Your Partner's Name and student #:Xinyue(Ingrid) Zeng A00937032
+ (Make sure you also specify on the Google Doc)
+*/
+const fs = require('fs')
+const express = require("express");
+
+let app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+
+app.get("/", (req, res) => res.render("pages/index"));
+
+app.get("/myForm", (req, res) => res.render("pages/myForm"));
+
+app.post("/myForm", (req, res) => {
+    let movieList = req.body.movies.split(",")
+    res.render("pages/index", { movies: movieList })
+})
+
+app.get("/myListQueryString", (req, res) => {
+    // Add  implementation here
+    let movieList = [req.query.movie1, req.query.movie2]
+    res.render("pages/index", { movies: movieList })
+});
+
+app.get("/search/:movieName", (req, res) => {
+    let search = req.params.movieName
+    debugger;
+    fs.readFile("./movieDescriptions.txt", (err, data) => {
+        if (err) {
+            console.log("Error reading movie")
+        } else {
+            let movieDescriptions = data.toString()
+            let movieList = movieDescriptions.split("\n")
+            let moviesObj = {}
+            movieList.forEach((movie) => {
+                currMovie = movie.split(":")
+                moviesObj[currMovie[0]] = currMovie[1]
+            })
+            foundMovie = null
+            if (search in moviesObj) {
+                foundMovie = moviesObj[search]
+            }
+            res.render("pages/searchResult", { foundMovie: [search, foundMovie] })
+        }
+    })
+
+
+});
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000 🚀");
+});
